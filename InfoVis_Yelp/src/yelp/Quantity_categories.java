83734b5a7ba212c7fcc08091c33b0b8833766a5e
Congwu Tao <congwu.tao@gmail.com>
2012-11-15 21:01:32 -0500

    Quantity_Categories is done
    "How many restaurants are within each these categories" has been solved

--- InfoVis_Yelp/src/yelp/Quantity_categories.java
+++ InfoVis_Yelp/src/yelp/Quantity_categories.java
@@ -1,5 +1,56 @@
 package yelp;
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Quantity_categories {
+	public static void main(String[] args) 	
+	{
+		try
+		{ 
+			String[] arrCat = {"Indian","Restaurants","Japanese","American (New)","Food","Farmers Market","American (Traditional)",
+					"Bakeries","Donuts","Coffee & Tea","Sandwiches","Vegetarian","Vegan","Burgers","Sushi Bars","Greek","Pizza",
+					"Mexican","Soup","Cajun/Creole","Bars","Pool Halls","Nightlife","Italian","Chinese","Ethiopian","Beer, Wine & Spirits",
+					"Seafood","Buffets","Thai","Mediterranean","Specialty Food","Health Markets","Grocery","Steakhouses","Wine Bars",
+					"Bed & Breakfast","Breakfast & Brunch","Diners","Desserts","Asian Fusion","Tea Rooms","Meat Shops","Fast Food",
+					"Delis","Barbeque","Sports Bars","Tex-Mex"}; 
+			
+			Map<String,Integer> arrCatMap = new HashMap<String,Integer>(); //calculate the number of each category
+			ObjectMapper mapper = new ObjectMapper();
+			List<Business_Prof> biz_profs = mapper.readValue(new File("C:\\Users\\Smile\\Documents\\GitHub\\vtvisyelp\\InfoVis_Yelp\\VT_jsons\\VT_business_profiles.json"), 
+					new TypeReference<List<Business_Prof>>(){});
+			
+			for(String arr:arrCat){
+				for(Business_Prof bp: biz_profs){
+					if(bp.getSchools().contains("Virginia Tech")){
+						if(bp.getCategories().contains(arr)){
+							if(arrCatMap.containsKey(arr)){
+								arrCatMap.put(arr, arrCatMap.get(arr)+1);
+							}else{
+								arrCatMap.put(arr, 1);
+							}
+						}
+					}
+					
+				}
+				
+			}
+			
+			
+			for(String arr:arrCatMap.keySet()){
+				System.out.println(arr+":"+arrCatMap.get(arr));
+			}
+			
+			
+		} 
+		catch (Exception e) 
+		{
+			System.out.println("IOException");
+			e.printStackTrace();
+		}
 
+		}
 }
